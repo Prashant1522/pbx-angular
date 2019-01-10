dbaa032ef8179b4d971792a80beba0187ea67e9d
Pooja Sharma <poojasharma5501@gmail.com>
2019-01-10 16:20:59 +0530

    dashboard

--- src/app/pages/billing/billing.component.ts
+++ src/app/pages/billing/billing.component.ts
@@ -6,42 +6,29 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
   styleUrls: ['./billing.component.css']
 })
 export class BillingComponent implements OnInit {
-  displayedColumns15: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource15 = new MatTableDataSource<PeriodicElement15>(ELEMENT_DATA15);
+  billingday= "28th of every month ";
+  paymentmethod ="Credit card";
+  accountbalance ="$0.00";
+  displayedColumns: string[] = ['photo', 'billdate', 'amount', 'payment']
+  dataSource  = new MatTableDataSource<PeriodicElement15>(ELEMENT_DATA15);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit() {
-    this.dataSource15.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
 export interface PeriodicElement15 {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  
+  billdate: string;
+  amount: string;
+  payment: string;
 }
 
 const ELEMENT_DATA15: PeriodicElement15[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {billdate:'Current Usage',amount:'$1.05', payment: 'To be billed: 06 /30 /2018'},
+  {billdate:'June 28, 2018', amount:'$177.54', payment: '($177.54)'},
+  {billdate:'May 28, 2018', amount:'$150.99', payment: '($150.99)'},
+  {billdate:'May 01, 2018', amount:'$64.95', payment: '($64.95)'},
 ];
