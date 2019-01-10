e78ba69cdb94c036431596c5ead29d87f71770af
geniusunil <geniusunil@gmail.com>
2019-01-10 12:35:48 +0530

    removed unnecessary comps

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,16 +9,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { MailboxComponent } from './mailbox/mailbox.component';
 import { DiscussionComponent } from './discussion/discussion.component';
-import { AccountingComponent } from './accounting/accounting.component';
-import { HrComponent } from './hr/hr.component';
+
+
 import { ReportsComponent } from './reports/reports.component';
-import { UtilitiesComponent } from './utilities/utilities.component';
-import { SurveyComponent } from './survey/survey.component';
+
 import { ClientComponent } from './client/client.component';
-import { VendorComponent } from './vendor/vendor.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { SalesComponent } from './sales/sales.component';
-import { SupportComponent } from './support/support.component';
+
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -68,16 +64,12 @@ import { Cards3Component } from './dashboard/dashboardComps/cards3/cards3.compon
       CalendarComponent,
        MailboxComponent,
         DiscussionComponent,
-        AccountingComponent, 
-        HrComponent, 
+        
         ReportsComponent,
-         UtilitiesComponent, 
-         SurveyComponent,
+       
+         
          ClientComponent ,
-         VendorComponent,
-         TasksComponent,
-         SalesComponent,
-         SupportComponent,
+      
          Cards3Component, 
         
     ],
