821b90808f65d0b51c870800f09e73eba5f5d0b9
Pooja Sharma <poojasharma5501@gmail.com>
2019-01-15 17:33:04 +0530

    dashboard

--- src/app/pages/connectivity/connectivity.component.ts
+++ src/app/pages/connectivity/connectivity.component.ts
@@ -6,8 +6,7 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
   styleUrls: ['./connectivity.component.css']
 })
 export class ConnectivityComponent implements OnInit {
-  outboundColumns1: string[] = ['did', 'cid', 'description', 'destination','pinning'];
-  outbound_dataelement1 = new MatTableDataSource<outboundPeriodicElement1>(OUTBOUND_ELEMENT_DATA1);
+  
   
   constructor() { }
 
@@ -15,20 +14,3 @@ export class ConnectivityComponent implements OnInit {
   }
 
 }
-export interface outboundPeriodicElement1 {
-  did: number;
- cid: string;
- description: string;
-  noofextension: number;
-  content: string;
-  destination:string;
-}
-
-const OUTBOUND_ELEMENT_DATA1: outboundPeriodicElement1[] = [
-  {did: 2143907281,cid: 'Any',description:'Hunter Kelly',destination:'Extension :',content:'This is demo Inbound Routes description here',noofextension: 9},
-  {did: 2146130380,cid: 'Any',description:'Dallas Fax',destination:'Misc Destinations : ',content:'Dallas Fax Temporary Forwarding Number ',noofextension: 2},
-  {did: 2146130500,cid: 'Any',description:'Dallas',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 4},
-  { did: 2146183420,cid: 'Any',description:'Frisco Wade',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 43},
-  {did: 2146124555,cid: 'Any',description:'Piano-Pkwy',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 9},
-  
-];
--- src/app/pages/connectivity/outbound/outbound.component.ts
+++ src/app/pages/connectivity/outbound/outbound.component.ts
@@ -1,5 +1,7 @@
-import {MatPaginator, MatTableDataSource} from '@angular/material';import { Component, OnInit } from '@angular/core';
-
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-outbound',
   templateUrl: './outbound.component.html',
@@ -8,7 +10,13 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';import { Comp
 export class OutboundComponent implements OnInit {
   outboundColumns1: string[] = ['did', 'cid', 'description', 'destination','pinning'];
   outbound_dataelement1 = new MatTableDataSource<outboundPeriodicElement1>(OUTBOUND_ELEMENT_DATA1);
+  to_destination = ['value0', 'value1','value2', 'value3'];
+  from_destination = ['value0', 'value1','value2', 'value3'];
+  musics=['value0', 'value1','value2', 'value3'];
+  alerts=['value0', 'value1','value2', 'value3'];
+  
 
+ 
   constructor() { }
 
   ngOnInit() {
@@ -30,5 +38,4 @@ const OUTBOUND_ELEMENT_DATA1: outboundPeriodicElement1[] = [
   {did: 2146130500,cid: 'Any',description:'Dallas',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 4},
   { did: 2146183420,cid: 'Any',description:'Frisco Wade',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 43},
   {did: 2146124555,cid: 'Any',description:'Piano-Pkwy',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 9},
-  
-];
+  ];
