814c38b9ae968d6d4ff95047fc6495535c89ae89
Pooja Sharma <poojasharma5501@gmail.com>
2019-01-15 12:13:46 +0530

    dashboard

--- src/app/pages/connectivity/outbound/outbound.component.ts
+++ src/app/pages/connectivity/outbound/outbound.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {MatPaginator, MatTableDataSource} from '@angular/material';import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-outbound',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./outbound.component.css']
 })
 export class OutboundComponent implements OnInit {
+  outboundColumns1: string[] = ['did', 'cid', 'description', 'destination','pinning'];
+  outbound_dataelement1 = new MatTableDataSource<outboundPeriodicElement1>(OUTBOUND_ELEMENT_DATA1);
 
   constructor() { }
 
@@ -13,3 +15,20 @@ export class OutboundComponent implements OnInit {
   }
 
 }
+export interface outboundPeriodicElement1 {
+  did: number;
+ cid: string;
+ description: string;
+  noofextension: number;
+  content: string;
+  destination:string;
+}
+
+const OUTBOUND_ELEMENT_DATA1: outboundPeriodicElement1[] = [
+  {did: 2143907281,cid: 'Any',description:'Hunter Kelly',destination:'Extension :',content:'This is demo Inbound Routes description here',noofextension: 9},
+  {did: 2146130380,cid: 'Any',description:'Dallas Fax',destination:'Misc Destinations : ',content:'Dallas Fax Temporary Forwarding Number ',noofextension: 2},
+  {did: 2146130500,cid: 'Any',description:'Dallas',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 4},
+  { did: 2146183420,cid: 'Any',description:'Frisco Wade',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 43},
+  {did: 2146124555,cid: 'Any',description:'Piano-Pkwy',destination:'Time Conditions : ',content:'This is demo Inbound Routes description here',noofextension: 9},
+  
+];
